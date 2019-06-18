4f5184cbbe4570bc6241b1f2114437b95f82067e
yaswanth1199 <51701963+yaswanth1199@users.noreply.github.com>
2019-06-18 14:18:52 +0530

    Update fact.c

--- fact.c
+++ fact.c
@@ -1,13 +1,20 @@
 #include<stdio.h>
 int main()
 {
-int x,i,m=1;
+int x,i,m;
 scanf("%d",&x);
+if(x==0)
+{
+    printf("1");
+}
+else
+{
 for(i=1;i<=x;i++)
 {
     m=i*m;
-
-
-}
+    ++i;
+    
+};
 printf("%d",m);
+}
 }
