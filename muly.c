cfaf7015408ca55a23c34887adf0587e155b7b13
yaswanth1199 <51701963+yaswanth1199@users.noreply.github.com>
2019-06-21 20:04:01 +0530

    Create muly.c

---
+++ muly.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int s,i=1,a;
+    scanf("%d",&s);
+    while(i!=6)
+    {
+       a=s*i;
+       printf("%d ",a);
+       ++i;
+    }
+
+        }
