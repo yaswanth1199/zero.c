968df74e4eebb91a55fd57e7cd314f352915dcaf
yaswanth1199 <51701963+yaswanth1199@users.noreply.github.com>
2019-06-21 14:42:16 +0530

    Create oddn.c

---
+++ oddn.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int s,y,x;
+    scanf("%d%d",&s,&y);
+    x=s+1;
+    if((s%2)==0)
+    {
+
+        while(x<y)
+        {
+          printf("%d ",x);
+            x=x+2;
+        }
+    }
+    else
+    {
+        while((x+1)<y)
+        {
+          printf("%d ",x+1);
+            x=x+2;
+        }
+    }
+
+
+
+
+}
