b18c8cba8d367d3ab63d98fcf2f76311f4efe31a
yaswanth1199 <51701963+yaswanth1199@users.noreply.github.com>
2019-07-11 20:15:13 +0530

    Create vv.c

---
+++ vv.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int a[100],b,i,j,c,k;
+    scanf("%d",&b);
+    for(i=0;i<b;++i)
+    {
+        scanf("%d",&a[i]);
+    }
+        for(i=0;i<b;++i)
+    {
+        for(j=0;j<b-1;++j)
+        {
+            if(a[j]>a[j+1])
+            {
+                c=a[j];
+                a[j]=a[j+1];
+                a[j+1]=c;
+            }
+        }
+    }
+    for(k=0;k<b;++k)
+    {
+        printf("%d ",a[k]);
+    }
+}
