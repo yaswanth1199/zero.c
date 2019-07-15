0e3f0ee686a019055412894085cb4d3392abef45
yaswanth1199 <51701963+yaswanth1199@users.noreply.github.com>
2019-07-15 11:05:18 +0530

    Create kh.c

---
+++ kh.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char a[100],i,d=0;
+    scanf("%[^\n]",a);
+    
+   
+     for(i=0;a[i]!='\0';++i)
+    {
+        if(a[i]==' ')
+        {
+        ++d;}
+    }
+    printf("%d",d);
+}
